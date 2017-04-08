39a449055c3e1092bb8a8eb1633228e2bb7391b4
lzw00000@qq.com <lzw00000@qq.com>
2017-04-08 21:45:20 +0800

    完成了快速排序的降序排序

--- quicksort/main.cpp
+++ quicksort/main.cpp
@@ -13,7 +13,7 @@ void swap(int &num0, int &num1) {
 }
 
 
-int parition(int arr[], int p, int r) {
+int paritionASC(int arr[], int p, int r) {
     int x = arr[r];
     int i = p - 1;
     for (int j = p; j < r; j++) {
@@ -26,21 +26,49 @@ int parition(int arr[], int p, int r) {
     return i + 1;
 }
 
+int paritionDSC(int arr[], int p, int r) {
+    int x = arr[r];
+    int i = r;
+    for (int j = r - 1; j >= p; j--) {
+        int num = arr[j];
+        if (num < x) {
+            i--;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i], arr[r]);
+    return i;
+}
 
-void quickSort(int arr[], int p, int r) {
+void quickSortASC(int arr[], int p, int r) {
     if (p < r) { // 数组中只有一个元素的时候就会结束
-        int q = parition(arr, p, r);
-        quickSort(arr, p, q - 1);
-        quickSort(arr, q + 1, r);
+        int q = paritionASC(arr, p, r);
+        quickSortASC(arr, p, q - 1);
+        quickSortASC(arr, q + 1, r);
     }
 }
 
 
+void quickSortDsc(int arr[], int p, int r) {
+    if (p < r) {
+        int q = paritionDSC(arr, p, r);
+        quickSortDsc(arr, p, q - 1);
+        quickSortDsc(arr, q + 1, r);
+    }
+}
+
 int main(int argc, const char *argv[]) {
     int arr[10] = {45, 23, 43, 23, 49, 66, 1, 3, 4, 10};
-    quickSort(arr, 0, 9);
+    quickSortASC(arr, 0, 9);
     for (int i = 0; i < 10; i++) {
-        cout << arr[i] << endl;
+        cout << arr[i] << "   ";
     }
+    cout << "\n";
+    int arr2[8] = {9, 8, 2, 11, 3, 10, 1, 7};
+    quickSortDsc(arr2, 0, 7);
+    for (int i = 0; i < 8; i++) {
+        cout << arr2[i] << "  ";
+    }
+    cout << "\n";
     return 0;
 }
