e0746797d3341a3d2d9ccd42bc42bbaf85440fd4
廖志伟 <zhiwei@liaozhiweideMacBook-Pro.local>
2017-04-26 19:41:32 +0800

    完成拟阵--贪心算法任务调度

---
+++ greedy_job_sechuler/cmake-build-debug/CMakeFiles/greedy_activity_sechuler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greedy_activity_sechuler.dir/main.cpp.o"
+  "greedy_activity_sechuler.pdb"
+  "greedy_activity_sechuler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greedy_activity_sechuler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- greedy_job_sechuler/main.cpp
+++ greedy_job_sechuler/main.cpp
@@ -4,48 +4,55 @@
 
 using namespace std;
 
-class Activity {
+class Job {
 public:
-
-    Activity(int s, int e) : startTime(s), endTime(e) {};
-    int startTime;
-    int endTime;
+    Job(int d, int w, int id) : d(d), w(w), id(id) {};
+    int d;
+    int w;
+    int id;
 };
+
+bool isInDedepend(vector<Job> &jobs) {
+    sort(jobs.begin(), jobs.end(), [](const Job &job1, const Job &job2) {
+        return job1.d < job2.d;
+    });
+    int i = 0;
+    for (vector<Job>::iterator it1 = jobs.begin(); it1 != jobs.end(); it1++) {
+        i++;
+        if (i > it1->d) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<Job> startScheule(vector<Job> &jobs) {
+    vector<Job> scheduedJobs;
+    sort(jobs.begin(), jobs.end(), [](const Job &job1, const Job &job2) {
+        return job1.w > job2.w;
+    });
+    for (vector<Job>::iterator it = jobs.begin(); it != jobs.end(); it++) {
+        scheduedJobs.push_back(*(it));
+        if (!isInDedepend(scheduedJobs)) {
+            scheduedJobs.erase(it);
+        }
+    }
+    for (vector<Job>::iterator it = scheduedJobs.begin(); it != scheduedJobs.end(); it++) {
+        cout << it->id << "\t" << it->w << endl;
+    }
+}
+
 
 int main() {
 
-    vector<Activity> activities;
-    activities.push_back(Activity(1, 4));
-    activities.push_back(Activity(3, 5));
-    activities.push_back(Activity(0, 6));
-    activities.push_back(Activity(5, 7));
-    activities.push_back(Activity(3, 8));
-    activities.push_back(Activity(5, 9));
-    activities.push_back(Activity(6, 10));
-    activities.push_back(Activity(8, 11));
-    activities.push_back(Activity(8, 12));
-    activities.push_back(Activity(2, 13));
-    activities.push_back(Activity(12, 14));
-
-    sort(activities.begin(), activities.end(), [](const Activity &a1, const Activity &a2) {
-        return a1.endTime < a2.endTime;
-    });
-
-    vector<Activity> choosedActivities;
-
-    choosedActivities.push_back(activities[0]);
-    int e = choosedActivities[0].endTime;
-    for (int i = 1; i < activities.size(); i++) {
-        if (e <= activities[i].startTime) {
-            e = activities[i].endTime;
-            choosedActivities.push_back(activities[i]);
-        }
-    }
-    for (vector<Activity>::iterator it = choosedActivities.begin(); it != choosedActivities.end(); it++) {
-        cout << it->startTime;
-        cout << "\t ";
-        cout << it->endTime;
-        cout << "\n";
-    }
+    vector<Job> jobs;
+    jobs.push_back(Job(4, 70, 1));
+    jobs.push_back(Job(2, 60, 2));
+    jobs.push_back(Job(1, 30, 5));
+    jobs.push_back(Job(4, 50, 3));
+    jobs.push_back(Job(4, 20, 6));
+    jobs.push_back(Job(6, 10, 7));
+    jobs.push_back(Job(3, 40, 4));
+    startScheule(jobs);
     return 0;
 }
