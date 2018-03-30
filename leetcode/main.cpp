742500fc44467d8abc45d4bd01ef58b9e8babbab
luckzhiwei <lzw00000@qq.com>
2018-03-30 22:56:55 +0800

    add the gitignore

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,68 +1,92 @@
-#include <stdio.h>
+#include <inttypes.h>
 #include <vector>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <limits.h>
+#include <algorithm>
+#include <math.h>
+#include "unordered_map"
+
 
 using namespace std;
 
-/** 请完成下面这个函数，实现题目要求的功能 **/
-/** 当然，你也可以不按照这个模板来作答，完全按照自己的想法来 ^-^  **/
-int minCost(vector<vector<int> > &costs) {
-    vector<vector<int> > dp(costs.size());
-    int N = costs.size();
-    int K = costs[0].size();
-    for (int i = 0; i < N; i++) {
-        dp[i].resize(costs[0].size());
+struct Conord {
+    int x;
+    int y;
+};
+
+
+char *strcpy(const char *src, char *dest) {
+    if (src == NULL || dest == NULL)
+        return NULL;
+    char *r = dest;
+    while ((*r++ = *src++) != '0');
+    return dest;
+}
+
+vector<Conord> func(vector<Conord> &vec, Conord e, int k) {
+    if (vec.size() < k) {
+        return vec;
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) {
+
+    make_heap(vec.begin(), vec.end() + k, [](Conord l1, Conord l2) {
+        double dis1 = sqrt(pow(l1.x - e.x, 2) + pow(l1.y - e.y, 2));
+        double dis2 = sqrt(pow(l2.x - e.x, 2) + pow(l2.y - e.y, 2));
+        return dis1 < dis2;
+    });
+
+    for (auto it = vec.begin() + k + 1; it != vec.end(); it++) {
+        double distMax = sqrt(pow(vec[0].x - e.x, 2) + pow(vec[0].y - e.y, 2));
+        double distIt = sqrt(pow(it->x - e.x, 2) + pow(it->y - e.y, 2));
+        if (distIt < distMax) {
+            vec.pop_back();
+            push_heap(vec.begin(), vec.end() + k, [](Conord l1, Conord l2) {
+                double dis1 = sqrt(pow(l1.x - e.x, 2) + pow(l1.y - e.y, 2));
+                double dis2 = sqrt(pow(l2.x - e.x, 2) + pow(l2.y - e.y, 2));
+                return dis1 < dis2;
+            });
+        } else {
+            it = vec.erase(it);
+        }
+    }
+    return vec;
+}
+
+vector<int> fun(int n) {
+    unordered_map map;
+    map[0] = 0;
+    map[1] = 1;
+    map[2] = 1;
+    map[3] = 2;
+    map[4] = 1;
+    map[5] = 2;
+    map[6] = 2;
+    map[7] = 3;
+    map[8] = 1;
+    map[9] = 2;
+    map[10] = 2;
+    map[11] = 3;
+    map[12] = 2;
+    map[13] = 3;
+    map[14] = 3;
+    map[15] = 4;
+
+    vector<int> ret;
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
+        for (int j = 0; j < 8; j++) {
+            int v = i & 15;
+            count += map[v];
+            i = i >> 4;
             if (i == 0) {
-                dp[i][j] = costs[i][j];
-            } else {
-                int lasti = i - 1;
-                int m_cost = 99999999;
-                for (int k = 0; k < K; k++) {
-                    if (j == k) continue;
-                    int thiscost = dp[lasti][k] + costs[i][j];
-                    m_cost = min(m_cost, thiscost);
-                }
-                dp[i][j] = m_cost;
+                break;
             }
         }
+        ret.push_back(count);
     }
-    int m_cost = 99999999;
-    for (int k = 0; k < N; k++) {
-        m_cost = min(m_cost, dp[N - 1][k]);
-    }
-    return m_cost;
+    return ret;
+
 }
 
-
-int main() {
-    int N, K;
-    int cost;
-    vector<vector<int> > costs;
-    string s;
-    istringstream is;
-
-    getline(cin, s);
-    is.str(s);
-    is >> N >> K;
-    for (int i = 0; i < N; i++) {
-        vector<int> t;
-        getline(cin, s);
-        is.clear();
-        is.str(s);
-        for (int j = 0; j < K; j++) {
-            is >> cost;
-            t.push_back(cost);
-        }
-        costs.push_back(t);
-    }
-    cout << minCost(costs) << endl;
+int main(int argc, char **argv) {
+    vector<Conord> vector1;
+    vector
     return 0;
 }
-
