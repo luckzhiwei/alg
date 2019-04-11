266618fc5a0cedab4c4c1bfcb57f3bccce47a679
luckzhiwei <lzw00000@qq.com>
2019-04-11 17:57:59 +0800

    m:2019.4.11,修复更新到mac seria后无法编译的错误

--- dyc_progming_max_common_str/cmake-build-debug/cmake_install.cmake
+++ dyc_progming_max_common_str/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zhiwei/alg/dyc_progming_max_str
+# Install script for directory: /Users/zhiwei/alg/dyc_progming_max_common_str
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zhiwei/alg/dyc_progming_max_str/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zhiwei/alg/dyc_progming_max_common_str/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
